e0a18f393c2f6dbfba3c1abfa6cae2340f4c800e
Artem Kononenko <kononenkoar@yandex.ru>
2022-11-08 00:03:11 +0300

    feat: task 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require("fs");
+const fsExtra = require("fs-extra");
+const path = require("path");
 
+const srcPath = path.join(__dirname, "files");
+const destPath = path.join(__dirname, "files-copy");
+
+fsExtra.remove(destPath, (err) => {});
+
+fs.readdir(srcPath, (error, data) => {
+  fs.mkdir(destPath, (err) => {});
+
+  data.forEach((file) => {
+    fs.readFile(`${srcPath}/${file}`, "utf8", (err, dataF) => {
+      fs.writeFile(`${destPath}/${file}`, dataF, () => {});
+    });
+  });
+});
