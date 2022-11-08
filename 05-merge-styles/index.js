44f1ee6cf3c75f917d1367b13ac042928f273500
Artem Kononenko <kononenkoar@yandex.ru>
2022-11-08 22:35:52 +0300

    feat: task 05-merge-styles done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const fsExtra = require("fs-extra");
+const path = require("path");
+
+const stylesPath = path.join(__dirname, "styles");
+const bundlePath = path.join(__dirname, "project-dist", "bundle.css");
+
+let writeableStream = fs.createWriteStream(bundlePath);
+
+fs.readdir(stylesPath, (err, data) => {
+  data.forEach((file) => {
+    let readableStream = fs.createReadStream(`${stylesPath}/${file}`);
+
+    let ext = path.extname(file);
+    if (ext === ".css") {
+      readableStream.pipe(writeableStream);
+    }
+  });
+});
