2b257a334c57668887a9cfdc40b9cfab78fc0350
Artem Kononenko <kononenkoar@yandex.ru>
2022-11-10 21:34:01 +0300

    refactor: delete module fs-extra

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
-const fsExtra = require("fs-extra");
 const path = require("path");
 
 const srcPath = path.join(__dirname, "files");
 const destPath = path.join(__dirname, "files-copy");
 
-fsExtra.remove(destPath, (err) => {});
+fs.rm(destPath, (err) => {});
 
 fs.readdir(srcPath, (error, data) => {
   fs.mkdir(destPath, (err) => {});
